d24871d58dea3818d502a24d3d2f8214eab37e23
Lindsey Handley <lindseyhandley@gmail.com>
2020-11-19 17:53:29 +0000

    added destroy rune

--- main.rkt
+++ main.rkt
@@ -1,18 +1,36 @@
-#lang codespells
+#lang at-exp codespells
 
 (require dev-runes/mod-info)
 
-(define-classic-rune (hello)
+; Note: SphereOverlapActors doesn't return all actors. (e.g. particle effects)
+; However, particle effects can be destroyed if they are childed to a parent with a mesh.
+; Or possibly any actor that "generates overlaps"
+; TODO: RESEARCH THIS
+
+(define-classic-rune (destroy-actors [radius 50])
   #:background "blue"
-  #:foreground (circle 40 'solid 'blue)
-  (spawn-mod-blueprint pak-folder mod-name "HelloWorld"))
+  #:foreground (circle 40 'solid 'red)
+  (unreal-js @~a{
+ (function(){
+   var actors = KismetSystemLibrary.SphereOverlapActors(GWorld, {X:@(current-x), Y:@(current-z), Z:@(current-y)}, @radius).OutActors
+
+   var destroy = function(a){
+     a.GetAttachedActors().OutActors.map(destroy)
+
+     a.DestroyActor()                         
+   }
+
+   actors.map(destroy)          
+  })()
+ }))
+
 
 (define-classic-rune-lang my-mod-lang #:eval-from main.rkt
-  (hello))
+  (destroy-actors))
 
 (module+ main
   (codespells-workspace ;TODO: Change this to your local workspace if different
-   (build-path (current-directory) ".." "CodeSpellsWorkspace"))
+   (build-path (current-directory) ".." ".."))
   
   (once-upon-a-time
    #:world (demo-world)
